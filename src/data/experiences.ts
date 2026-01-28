524267380fb586b2440b00e352e58f174045eb66
mehdihilali <elhilali.elmehdi.edu@gmail.com>
2026-01-28 19:27:42 +0100

    Fixing some tech

--- src/data/experiences.ts
+++ src/data/experiences.ts
@@ -5,12 +5,12 @@ export const experiences: Experience[] = [
         id: '1',
         company: 'Clever Council',
         position: {
-            en: 'Full Stack Developer (Java / React)',
-            fr: 'Développeur Full Stack (Java / React)'
+            en: 'Full Stack Developer Intern (Java / React)',
+            fr: 'Stagiaire Développeur Full Stack (Java / React)'
         },
         duration: {
-            en: 'Aug 2025 - Present',
-            fr: 'Août 2025 - Présent'
+            en: 'Aug 2025 - Jan 2026',
+            fr: 'Août 2025 - Jan 2026'
         },
         location: {
             en: 'Casablanca, Morocco',
@@ -18,26 +18,25 @@ export const experiences: Experience[] = [
         },
         description: {
             en: [
-                'Developed Spring Boot microservices and React frontend for the Ministry of Health portal',
+                'Developed backend microservices and React frontend for the Ministry of Health portal',
                 'Designed and integrated an advanced Document Management System (GED) with secure storage and search',
                 'Built an intelligent chatbot for user assistance using Rasa',
                 'Implemented CI/CD pipelines using GitLab for automated testing and reliable deployments',
-                'Deployed Spring Boot backend using Nginx (reverse proxy) and React frontend using PM2',
+                'Deployed backend using Nginx (reverse proxy) and React frontend using PM2',
                 'Migrated the database from MySQL to SQL Server using tools such as MMSA and SSMS to ensure optimal performance and data integrity',
-                'Automated large-scale data extraction and transfer from the old Ministry website to the new platform using custom bulk download/upload scripts, improving migration efficiency by 40%'
+                'Automated large-scale data extraction and transfer from the old Ministry website to the new platform using custom bulk download/upload scripts, improving migration efficiency by 80%'
             ],
             fr: [
-                'Développement de microservices Spring Boot et d’un frontend React pour le portail du Ministère de la Santé',
+                'Développement de microservices backend et d’un frontend React pour le portail du Ministère de la Santé',
                 'Conception et intégration d’un système avancé de gestion documentaire (GED) avec stockage et recherche sécurisés',
                 'Création d’un chatbot intelligent pour l’assistance utilisateur utilisant Rasa',
                 'Mise en œuvre de pipelines CI/CD utilisant GitLab pour des tests automatisés et des déploiements fiables',
-                'Déploiement du backend Spring Boot utilisant Nginx (reverse proxy) et du frontend React utilisant PM2',
+                'Déploiement du backend utilisant Nginx (reverse proxy) et du frontend React utilisant PM2',
                 'Migration de la base de données de MySQL vers SQL Server en utilisant des outils tels que MMSA et SSMS pour garantir performance optimale et intégrité des données',
-                'Automatisation du téléchargement et de l’upload massif (scraping) depuis l’ancien site du Ministère vers la nouvelle plateforme à l’aide de scripts de bulk download/upload, augmentant l’efficacité de la migration de 40%'
+                'Automatisation du téléchargement et de l’upload massif (scraping) depuis l’ancien site du Ministère vers la nouvelle plateforme à l’aide de scripts de bulk download/upload, augmentant l’efficacité de la migration de 80%'
             ]
         },
         technologies: [
-            'Spring Boot',
             'React',
             'Microservices',
             'GitLab CI/CD',
@@ -51,8 +50,6 @@ export const experiences: Experience[] = [
             'SSMS',
             'Web Scraping',
             'Bulk Scripts',
-            'PHP',
-            'Laravel'
         ],
     },
     {
@@ -76,19 +73,20 @@ export const experiences: Experience[] = [
                 'Built Spring Boot microservices with asynchronous communication using RabbitMQ',
                 'Secured authentication and authorization using Keycloak',
                 'Documented APIs with Swagger/OpenAPI',
-                'Achieved 75% test coverage using JUnit and Mockito'
+                'Achieved 80% test coverage using JUnit and Mockito'
             ],
             fr: [
                 'Conception et développement de Proway, une plateforme d\'orientation professionnelle propulsée par l\'IA',
                 'Construction de microservices Spring Boot avec communication asynchrone utilisant RabbitMQ',
                 'Sécurisation de l\'authentification et de l\'autorisation utilisant Keycloak',
                 'Documentation des API avec Swagger/OpenAPI',
-                'Atteinte de 75% de couverture de tests utilisant JUnit et Mockito'
+                'Atteinte de 80% de couverture de tests utilisant JUnit et Mockito'
             ]
         },
         technologies: [
             'Spring Boot',
             'React',
+            'LangChain',
             'RabbitMQ',
             'Keycloak',
             'Swagger',
